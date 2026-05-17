641d5fc660d6451eacedbc548cd68c0b247837bf
naima-mohammed <n.aldasoqi@uot.edu.ly>
2026-05-17 17:20:38 +0200

    edit

--- qq.cpp
+++ qq.cpp
@@ -65,7 +65,6 @@ public:
             total += probs[i];
         }
 
-        // Normalize
         //2.Determine the selection probabilities for each individual
         for (int i = 0; i < probs.size(); i++)
         {
@@ -84,18 +83,14 @@ public:
 
             //3.Generate a random selection point
             double r = (double) rand() / RAND_MAX;
-
             double cumulative = 0;
 
             //4.Select an individual based on the random selection point
             for (int i = 0; i < N; i++) {
 
                 cumulative += probs[i];
-
                 if (r <= cumulative) {
-
                     selected_population.push_back(population[i]);
-
                     break;
                 }
             }
@@ -107,17 +102,10 @@ public:
 
     // Print population
     void printPopulation() {
-
         for (int i = 0; i < population.size(); i++) {
-
-            cout << "[ ";
-
-            for (int j = 0; j < population[i].size(); j++) {
-
-                cout << population[i][j] << " ";
-            }
-
-            cout << "]" << endl;
+           cout << "[ ";
+           for (int j = 0; j < population[i].size(); j++)
+            {cout << population[i][j] << " "; }  cout << "]" << endl;
         }
     }
 
@@ -126,11 +114,8 @@ public:
 
         //Calculate the total fitness of the entire population
         vector<int> fitness = calcFitness();
-
         cout << "\nFitness Values:\n";
-
         for (int i = 0; i < fitness.size(); i++) {
-
             cout << fitness[i] << endl;
         }
     }
@@ -139,21 +124,13 @@ public:
     void printSelectedPopulation() {
 
         vector<int> fitness = calcFitness();
-
         vector<vector<int>> selected = selection(fitness);
-
         cout << "\nSelected Population:\n";
-
         for (int i = 0; i < selected.size(); i++) {
-
             cout << "[ ";
-
             for (int j = 0; j < selected[i].size(); j++) {
-
                 cout << selected[i][j] << " ";
-            }
-
-            cout << "]" << endl;
+            } cout << "]" << endl;
         }
     }
 };
@@ -170,10 +147,7 @@ int main() {
     {2,4,4,1,5,1,2,4},
     {3,2,5,4,3,2,1,3}
     };
-
-
-    /*
-// مثال فيديو 
+   /*// مثال فيديو 
      vector<vector<int>> people = {
     {6,1,7,3,6,1,4,0},
     {3,4,0,6,6,1,2,3},
@@ -181,16 +155,10 @@ int main() {
     {0,2,2,7,4,4,7, 3}
     };
 */
-
     Population initial_population(people);
-
     cout << "Initial Population:\n";
-
     initial_population.printPopulation();
-
     initial_population.printFitness();
-
     initial_population.printSelectedPopulation();
-
     return 0;
 }
